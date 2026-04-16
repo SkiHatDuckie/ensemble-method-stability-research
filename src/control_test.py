5b8f40cb9209db3c357525b37f11a7979ec61754
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-04-16 18:14:04 -0400

    Add rudimentary train-test loop

--- src/control_test.py
+++ src/control_test.py
@@ -1,6 +1,7 @@
 # src/control_test.py
 # Used for training/evaluating models on unaltered data
 import pandas as pd
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 from models import BaseLearner, AdaBoost, GradientBoosting, RandomForest
@@ -16,19 +17,22 @@ if __name__ == "__main__":
     Add Feature Selection Here (?)
     """
 
-    model = BaseLearner()
-    adaboost = AdaBoost(model)
+    learner = BaseLearner()
+    adaboost = AdaBoost(learner)
     gradient_boosting = GradientBoosting()
     random_forest = RandomForest()
 
     """TODO
     Add Training Process
     """
-    train_data, test_data = train_test_split(data, test_size=0.2, random_state=15)
-    train_inputs = train_data.iloc[:, :-1]
-    train_targets = train_data[["Churn"]]
-    test_inputs = test_data.iloc[:, :-1]
-    test_targets = test_data[["Churn"]]
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1:]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=15)
+
+    learner.model.fit(X_train, y_train)
+    y_pred = learner.model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Accuracy = {accuracy:.2f}")
 
     """TODO
     Add Evaluation Process
