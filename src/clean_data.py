c6a9b7d8bb686d6ee17e5bb4bf27eccca1fdf3b3
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-04-17 18:30:54 -0400

    Remap all categories to numeric values

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,17 +1,22 @@
 # src/clean_data.py
 # Cleaning and preprocessing the raw CSV for testing.
 # Run this before running any tests.
+import json
 
 import pandas as pd
 
 if __name__ == "__main__":
-    data_path = "data/WA_Fn-UseC_-Telco-Customer-Churn.csv"
-    data = pd.read_csv(data_path, delimiter=",")
+    data = pd.read_csv("data/WA_Fn-UseC_-Telco-Customer-Churn.csv", delimiter=",")
 
     """TODO
     Do Preprocessing
     """
 
-    clean_data_path = "data/clean-telco-churn.csv"
-    with open(clean_data_path, "w", encoding="utf-8") as f:
+    # Remap all non-numeric columns to numeric using lookup tables
+    with open("src/category_remap.json", "r", encoding="utf-8") as f:
+        tables = json.load(f)
+        for col in tables.keys():
+            data[col] = data[col].map(tables[col])
+
+    with open("data/clean-telco-churn.csv", "w", encoding="utf-8") as f:
         data.to_csv(f)
