8324955d4c3c741fb4bf97b7a5cf0f30256ac9ba
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-04-19 18:14:11 -0400

    Switch to banknote dataset

--- src/control_test.py
+++ src/control_test.py
@@ -2,23 +2,24 @@
 # Used for training/evaluating models on unaltered data
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from ucimlrepo import fetch_ucirepo
 
 from models import BaseLearner, AdaBoost, GradientBoosting, RandomForest
 
 if __name__ == "__main__":
-    data_path = "data/clean-telco-churn.csv"
-    try:
-        data = pd.read_csv(data_path, delimiter=",")
-    except FileNotFoundError:
-        print(f"ERROR: Cleaned dataset '{data_path}' not found. Did you run module clean_data?")
+    banknote_authentication = fetch_ucirepo(id=267)
+    # try:
+    #     data = pd.read_csv(data_path, delimiter=",")
+    # except FileNotFoundError:
+    #     print(f"ERROR: Cleaned dataset '{data_path}' not found. Did you run module clean_data?")
 
     learner = BaseLearner()
     adaboost = AdaBoost(learner)
     gradient_boosting = GradientBoosting()
     random_forest = RandomForest()
 
-    X = data.iloc[:, :-1]
-    y = data.iloc[:, -1:]
+    X = banknote_authentication.data.features
+    y = banknote_authentication.data.targets
 
     training_scores = []
     testing_scores = []
@@ -28,16 +29,12 @@ if __name__ == "__main__":
         learner.model.fit(X_train, y_train)
         training_scores.append(learner.model.score(X_train, y_train))
         testing_scores.append(learner.model.score(X_test, y_test))
-        # print(f"Seed {seed}: Training Accuracy = {training_scores[-1]*100:2.1f}%")
-        # print(f"Seed {seed}: Testing Accuracy = {testing_scores[-1]*100:2.1f}%")
+        print(f"Seed {seed}: Training Accuracy = {training_scores[-1]*100:2.1f}%")
+        print(f"Seed {seed}: Testing Accuracy = {testing_scores[-1]*100:2.1f}%")
     avg_training_score = sum(training_scores)/len(training_scores)
     avg_testing_score = sum(testing_scores)/len(testing_scores)
     print(f"AVG Training Accuracy: {avg_training_score*100:2.1f}%")
     print(f"AVG Testing Accuracy: {avg_testing_score*100:2.1f}%")
-
-    """TODO
-    Add Evaluation Process
-    """
 
     """TODO
     Save Results to a File
