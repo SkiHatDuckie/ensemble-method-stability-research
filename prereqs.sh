321e5671c60056a7ebad910cd470fda896ad13e4
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-04-19 18:16:45 -0400

    Restructure project

--- prereqs.sh
+++
@@ -1,8 +0,0 @@
-#! bin/bash
-echo Downloading Telco customer churn datset to "$PWD"/data/
-DATA_PATH=data/
-FILE="$DATA_PATH"/telco-customer-churn.zip
-curl -L -o "$FILE"\
-  https://www.kaggle.com/api/v1/datasets/download/blastchar/telco-customer-churn\
-  --create-dirs
-unzip "$FILE" -d "$DATA_PATH"
