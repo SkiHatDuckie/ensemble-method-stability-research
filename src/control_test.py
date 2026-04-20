0de26870f84ae73ce90ae791456a8a147993f44b
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-04-20 19:09:10 -0400

    Reorganize and format metrics

--- src/control_test.py
+++ src/control_test.py
@@ -11,53 +11,55 @@ from metrics import Metric, MetricActions
 from models import BaseLearner, AdaBoost, GradientBoosting, RandomForest
 
 def create_results_filepath(location, prefix="results") -> Path:
-    filename = f"{prefix}_{time.localtime()}.res"
+    filename = f"{prefix}_{time.strftime('%Y%m%d-%H%M%S')}.res"
     filepath = location+filename
     os.makedirs(os.path.dirname(filepath), exist_ok=True)
     return filepath
 
-def write_results(filepath, *metrics):
-    pass
+def write_results(filepath, method, num_runs, *metrics_):
+    with open(filepath, "a", encoding="utf-8") as f:
+        f.write(f"Method: {method}\n")
+        f.write(f"runs: {num_runs}\n")
+        for metric in metrics_:
+            f.write(f"{metric}")
+        f.write("\n")
 
-def avg(list_) -> float:
-    """Returns the mean from a list of numbers `list_`."""
-    return sum(list_) / len(list_)
-
-def train_test_loop(model, num_runs, results_path) -> None:
-    training_scores = Metric(name="training accuracy", actions=MetricActions.AVERAGE)
-    testing_scores = Metric(name="testing accuracy", actions=MetricActions.AVERAGE)
+def train_test_loop(method, num_runs, results_path) -> None:
+    training_scores = Metric(name="training accuracy",
+                             actions=[MetricActions.PERCENT_AVERAGE],
+                             decimal_precision=2)
+    testing_scores = Metric(name="testing accuracy",
+                            actions=[MetricActions.PERCENT_AVERAGE],
+                            decimal_precision=2)
     training_times = Metric(name="training time",
-                            actions=[MetricActions.AVERAGE, MetricActions.TOTAL])
+                            actions=[MetricActions.AVERAGE, MetricActions.TOTAL],
+                            suffix="sec")
     results_metrics = [training_scores, testing_scores, training_times]
+
     for seed in range(0, num_runs):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
                                                             random_state=seed)
         time_start = time.perf_counter()
-        model.fit(X_train, y_train)
+        method.model.fit(X_train, y_train)
 
         training_times.data.append(time.perf_counter() - time_start)
-        training_scores.data.append(model.score(X_train, y_train))
-        testing_scores.data.append(model.score(X_test, y_test))
+        training_scores.data.append(method.model.score(X_train, y_train))
+        testing_scores.data.append(method.model.score(X_test, y_test))
 
-    write_results(results_path, *results_metrics)
+    write_results(results_path, method, num_runs, *results_metrics)
 
 if __name__ == "__main__":
     banknote_authentication = fetch_ucirepo(id=267)
     X = banknote_authentication.data.features
     y = banknote_authentication.data.targets.values.ravel()
-
     results_path = create_results_filepath("results/", prefix="control")
 
     learner = BaseLearner()
     adaboost = AdaBoost(learner.model)
     gradient_boosting = GradientBoosting()
     random_forest = RandomForest()
-    # methods = (learner, adaboost, gradient_boosting, random_forest)
-    methods = (learner,)
+    methods = (learner, adaboost, gradient_boosting, random_forest)
     runs = 100
     for method in methods:
         print(f"Running method: {method}")
-        train_test_loop(method.model, runs, results_path)
-        # print(f"runs: {runs}\t time: {time.perf_counter() - time_start:.3f} sec")
-        # print(f"avg. training accuracy: {avg(training_scores)*100:.2f}%")
-        # print(f"avg. testing accuracy: {avg(testing_scores)*100:.2f}%\n")
+        train_test_loop(method, runs, results_path)
--- src/metrics.py
+++ src/metrics.py
@@ -1,15 +1,36 @@
 # src/metrics.py
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import enum
+from typing import List
+
+from utils import avg
 
 
 class MetricActions(enum.Enum):
     AVERAGE = enum.auto()
     TOTAL = enum.auto()
+    PERCENT_AVERAGE = enum.auto()
 
 
 @dataclass
 class Metric:
     name: str
-    data: list=[]
-    actions: MetricActions|list[MetricActions]
+    actions: List[MetricActions]
+    suffix: str=""
+    decimal_precision: int=3
+    data: list=field(default_factory=list)
+
+    def __str__(self):
+        res = ""
+        for action in self.actions:
+            match action:
+                case MetricActions.AVERAGE:
+                    res += f"avg. {self.name}: \
+{avg(self.data):.{self.decimal_precision}f} {self.suffix}\n"
+                case MetricActions.TOTAL:
+                    res += f"tot. {self.name}: \
+{sum(self.data):.{self.decimal_precision}f} {self.suffix}\n"
+                case MetricActions.PERCENT_AVERAGE:
+                    res += f"avg. {self.name}: \
+{avg(self.data)*100:.{self.decimal_precision}f}%\n"
+        return res
