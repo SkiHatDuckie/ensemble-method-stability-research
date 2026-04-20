553779fa891355d220eac655fe65970370a9f2ab
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-04-19 20:24:03 -0400

    Test all models (control)

--- src/control_test.py
+++ src/control_test.py
@@ -13,27 +13,32 @@ if __name__ == "__main__":
     #     print(f"ERROR: Cleaned dataset '{data_path}' not found. Did you run module clean_data?")
 
     learner = BaseLearner()
-    adaboost = AdaBoost(learner)
+    adaboost = AdaBoost(learner.model)
     gradient_boosting = GradientBoosting()
     random_forest = RandomForest()
 
     X = banknote_authentication.data.features
     y = banknote_authentication.data.targets
+    y = y.values.ravel()
 
-    training_scores = []
-    testing_scores = []
-    for seed in range(0, 100):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
+    methods = (learner, adaboost, gradient_boosting, random_forest)
+    for method in methods:
+        training_scores = []
+        testing_scores = []
+        print(f"Method/Model: {method}")
+        for seed in range(0, 100):
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
+            
 
-        learner.model.fit(X_train, y_train)
-        training_scores.append(learner.model.score(X_train, y_train))
-        testing_scores.append(learner.model.score(X_test, y_test))
-        print(f"Seed {seed}: Training Accuracy = {training_scores[-1]*100:2.1f}%")
-        print(f"Seed {seed}: Testing Accuracy = {testing_scores[-1]*100:2.1f}%")
-    avg_training_score = sum(training_scores)/len(training_scores)
-    avg_testing_score = sum(testing_scores)/len(testing_scores)
-    print(f"AVG Training Accuracy: {avg_training_score*100:2.1f}%")
-    print(f"AVG Testing Accuracy: {avg_testing_score*100:2.1f}%")
+            method.model.fit(X_train, y_train)
+            training_scores.append(method.model.score(X_train, y_train))
+            testing_scores.append(method.model.score(X_test, y_test))
+            # print(f"Seed {seed}: Training Accuracy = {training_scores[-1]*100:2.1f}%")
+            # print(f"Seed {seed}: Testing Accuracy = {testing_scores[-1]*100:2.1f}%")
+        avg_training_score = sum(training_scores)/len(training_scores)
+        avg_testing_score = sum(testing_scores)/len(testing_scores)
+        print(f"AVG Training Accuracy: {avg_training_score*100:2.1f}%")
+        print(f"AVG Testing Accuracy: {avg_testing_score*100:2.1f}%")
 
     """TODO
     Save Results to a File
--- src/models.py
+++ src/models.py
@@ -8,17 +8,29 @@ class BaseLearner:
     def __init__(self) -> None:
         self.model = tree.DecisionTreeClassifier(max_depth=1)
 
+    def __str__(self) -> str:
+        return "DecisionTreeClassifier"
+
 
 class AdaBoost:
     def __init__(self, model) -> None:
-        self.method = AdaBoostClassifier(estimator=model, n_estimators=100)
+        self.model = AdaBoostClassifier(estimator=model, n_estimators=100)
+    
+    def __str__(self) -> str:
+        return "AdaBoostClassifier"
 
 
 class GradientBoosting:
     def __init__(self) -> None:
-        self.method = HistGradientBoostingClassifier()
+        self.model = HistGradientBoostingClassifier()
+    
+    def __str__(self) -> str:
+        return "HistGradientBoostingClassifier"
 
 
 class RandomForest:
     def __init__(self) -> None:
-        self.method = RandomForestClassifier(n_estimators=100)
+        self.model = RandomForestClassifier(n_estimators=100)
+    
+    def __str__(self) -> str:
+        return "RandomForestClassifier"
