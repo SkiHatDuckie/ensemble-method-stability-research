a6c8eb8738519f0ec293a600ab5463b9e8f64c09
SkiHatDuckie <SkiHatDuckie@gmail.com>
2026-04-25 15:47:28 -0400

    Add argparse

--- src/noise_demo.py
+++ src/noise_demo.py
@@ -1,12 +1,27 @@
 # src/data_analysis.py
+import argparse
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 
+def get_sample(df, frac:float, sample_type, seed=0) -> pd.DataFrame:
+    match sample_type:
+        case "random":
+            return df.sample(frac=frac, random_state=seed)
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "noise demo",
+        description="""Generates a diagram of the different noise algorithms applyed in
+the experiment.""")
+    parser.add_argument("-frac", type=float, default=0.10)
+    args = parser.parse_args()
+    frac = args.frac
+
     np.random.seed(3)
-    df = pd.DataFrame(np.random.rand(50, 2), columns=["X", "Y"])
+    df = pd.DataFrame(np.random.rand(100, 2), columns=["X", "Y"])
     df["class"] = (df["Y"] >= df["X"]).astype(int)
 
     fig, axes = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(10, 4))
@@ -18,7 +33,7 @@ if __name__ == "__main__":
     sns.scatterplot(data=df, x="X", y="Y", hue="class", s=75, ax=axes[0])
 
     axes[1].set_title("Random Label Noise")
-    label_noise_sample = df.sample(frac=0.15, random_state=0)
+    label_noise_sample = get_sample(df, frac, "random")
     label_noise_sample["class"] = 1 - label_noise_sample["class"]
     df.loc[label_noise_sample.index, :] = label_noise_sample
 
